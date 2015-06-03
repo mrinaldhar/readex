78ee5314e766ae019c0c00c3289d719a0f6dc063
Chanakya <chanakya.malireddy@students.iiit.ac.in>
2015-06-03 16:14:20 +0530

    Correction: initialize index=1 and increment index inside loop

--- parser.py
+++ parser.py
@@ -3,9 +3,10 @@ def parse(string):
 	brack_count = 0;
 	escape = 0;
 	atom = ''
-
+	
+	#CHECK IF STRING IS A GROUP
 	if string[0] == '(':
-		index = 0
+		index = 1
 		for each in string:
 			if each == '(':
 				brack_count += 1
@@ -13,6 +14,7 @@ def parse(string):
 				brack_count -= 1
 			if brack_count == 0:
 				break
+			index += 1
 		if index == len(string):
 			string = string[1:-1]
 
